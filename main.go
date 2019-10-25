317496144867bc64f71eb2c401b9b1bf7c72dec9
Kim, Jong-kook <kjk15881588@gmail.com>
2019-10-25 15:02:13 +0900

    csv데이터 xlsx변환 기능 추가

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"C"
+
+	xl "./smok95/xlsx"
+)
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+// Parse 실행인자 해석
+func initFlag(opt *xl.Options) {
+	flag.StringVar(&opt.Outfile, "out", "", "Path to the xlsx output file.")
+	flag.BoolVar(&opt.SkipEmptyLine, "skip-empty-line", false, "엑셀파일로 변환시 빈줄은 제거하고 변환한다.")
+	flag.StringVar(&opt.SheetName, "sheet-name", "Sheet1", "시트명")
+	flag.StringVar(&opt.FontName, "font-name", "맑은 고딕", "Default font name")
+	flag.IntVar(&opt.FontSize, "font-size", 11, "Default font size")
+}
+
+func main() {
+	var opt xl.Options
+	var csvFile string
+
+	flag.StringVar(&csvFile, "in", "", "Path to the csv input file.")
+	initFlag(&opt)
+
+	flag.Parse()
+
+	buf, err := ioutil.ReadFile(csvFile)
+	var csv string
+	if err != nil {
+		fmt.Printf(err.Error())
+		return
+	}
+
+	csv = string(buf)
+	xl.ConvertFromCSV(csv, opt)
+}
+
+//export csv2xlsx
+func csv2xlsx(pszText *C.char, pszOptions *C.char) C.long {
+	var opt xl.Options
+	text := C.GoString(pszText)
+	cmdline := C.GoString(pszOptions)
+	initFlag(&opt)
+	args := strings.Split(cmdline, "\n")
+	flag.CommandLine.Parse(args)
+	ret := xl.ConvertFromCSV(text, opt)
+	return C.long(ret)
+}
+
+/*
+go env 로 아래와 같이 설정되어 있는지확인
+set GOARCH=386
+set CGO_ENABLED=1
+// 빌드 방법
+	* dll
+		go build -buildmode=c-shared -o xlsx.dll main.go
+	* exe
+		go build -o xlsx.exe main.go
+*/
--- test/xlsx.test/xlsx.test.cpp
+++ test/xlsx.test/xlsx.test.cpp
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <atlstr.h>
 #include <string>
+#include <iostream>
+#include <fstream>
 
-typedef int (*fnText2xlsx)(const char* pszText, const char* pszOutFile);
+typedef int (*fnCsv2xlsx)(const char* pszText, const char* pszOutFile);
 
 std::string mbstr2utf8(const char* pszText) {
 	std::wstring sUnicode = CA2W(pszText).m_psz;
@@ -13,20 +15,35 @@ std::string mbstr2utf8(const char* pszText) {
 	return sUtf8;
 }
 
-int main()
-{
+std::string readfile(const char* filename) {
+	std::string lines;
+	std::ifstream file(filename);
+	if (!file.is_open())
+		return lines;
+	std::string line;
+	while (std::getline(file, line)) {
+		//if (line.length() == 0)
+		//	line = " ";	// empty line인 경우 공백추가, go encoding/csv에서 empty line은 skip처리되는 문제
+		lines += line + "\n";
+	}		
+	file.close();
+	return lines;
+}
+
+void test() {
 	int result = -1;
-	const char* pszText = "col1\tcol2\tcol3\nvalue1\tvalue2\tvalue3";
+	std::string sSrc = readfile("ansi.sample.csv");
+
 	if (HMODULE hDll = LoadLibrary("xlsx.dll")) {
 
-		fnText2xlsx text2xlsx = (fnText2xlsx)GetProcAddress(hDll, "text2xlsx");
+		fnCsv2xlsx text2xlsx = (fnCsv2xlsx)GetProcAddress(hDll, "csv2xlsx");
 		if (text2xlsx) {
-			std::string sUtf8 = mbstr2utf8("안녕.xlsx");
-			std::string sText = mbstr2utf8(pszText);
+			std::string sUtf8 = mbstr2utf8("-out=ansi.sample.xlsx");
+			std::string sText = mbstr2utf8(sSrc.c_str());
 			result = text2xlsx(sText.c_str(), sUtf8.c_str());
-		}			
+		}
 		else
-			puts("Failed to GetProcAddress('text2xlsx')");
+			puts("Failed to GetProcAddress('csv2xlsx')");
 
 		FreeLibrary(hDll);
 	}
@@ -35,6 +52,13 @@ int main()
 	}
 
 	printf("엑셀파일 생성결과 : %d\n", result);
+}
+
+int main()
+{
+	for (int i = 0; i < 10; i++) {
+		test();
+	}
 
 	return 0;
 }
